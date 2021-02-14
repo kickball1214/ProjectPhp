69c162faa8aa5efba0e046106a69f49c03ba6775
kickball1214 <64017956+kickball1214@users.noreply.github.com>
2021-02-14 20:04:41 +0200

    My first Commit

---
+++ fun/app/Http/Controllers/Admin/ArticleCrudController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\ArticleRequest;
+use Backpack\CRUD\app\Http\Controllers\CrudController;
+use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+
+/**
+ * Class ArticleCrudController
+ * @package App\Http\Controllers\Admin
+ * @property-read \Backpack\CRUD\app\Library\CrudPanel\CrudPanel $crud
+ */
+class ArticleCrudController extends CrudController
+{
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
+
+    private function getFieldsData($show = FALSE) {
+        return [
+            [
+                'name'=> 'title',
+                'label' => 'Title',
+                'type'=> 'text'
+            ],
+            [
+                'name' => 'content',
+                'label' => 'Content',
+                'type' => ($show ? "textarea": 'ckeditor'),
+            ],
+            [    // Select2Multiple = n-n relationship (with pivot table)
+                'label'     => "Tags",
+                'type'      => ($show ? "select": 'select2_multiple'),
+                'name'      => 'tags', // the method that defines the relationship in your Model
+// optional
+                'entity'    => 'tags', // the method that defines the relationship in your Model
+                'model'     => "App\Models\Tag", // foreign key model
+                'attribute' => 'name', // foreign key attribute that is shown to user
+                'pivot'     => true, // on create&update, do you need to add/delete pivot table entries?
+            ],
+            [
+                'label' => "Article Image",
+                'name' => "image",
+                'type' => 'image',
+                'crop' => true, // set to true to allow cropping, false to disable
+                'aspect_ratio' => 1, // omit or set to 0 to allow any aspect ratio
+            ]
+        ];
+    }
+    /**
+     * Configure the CrudPanel object. Apply settings to all operations.
+     * 
+     * @return void
+     */
+    public function setup()
+    {
+        CRUD::setModel(\App\Models\Article::class);
+        CRUD::setRoute(config('backpack.base.route_prefix') . '/article');
+        CRUD::setEntityNameStrings('article', 'articles');
+        $this->crud->addFields($this->getFieldsData());
+    }
+
+    /**
+     * Define what happens when the List operation is loaded.
+     * 
+     * @see  https://backpackforlaravel.com/docs/crud-operation-list-entries
+     * @return void
+     */
+    protected function setupListOperation()
+    {
+    $this->crud->set('show.setFromDb', false);
+    $this->crud->addColumns($this->getFieldsData(TRUE));
+    }
+
+    /**
+     * Define what happens when the Create operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-create
+     * @return void
+     */
+    protected function setupCreateOperation()
+    {
+        CRUD::setValidation(ArticleRequest::class);
+
+        CRUD::setFromDb(); // fields
+
+        /**
+         * Fields can be defined using the fluent syntax or array syntax:
+         * - CRUD::field('price')->type('number');
+         * - CRUD::addField(['name' => 'price', 'type' => 'number'])); 
+         */
+    }
+
+    /**
+     * Define what happens when the Update operation is loaded.
+     * 
+     * @see https://backpackforlaravel.com/docs/crud-operation-update
+     * @return void
+     */
+    protected function setupUpdateOperation()
+    {
+        $this->setupCreateOperation();
+    }
+    protected function setupShowOperation()
+{
+    // by default the Show operation will try to show all columns in the db table,
+    // but we can easily take over, and have full control of what columns are shown,
+    // by changing this config for the Show operation
+    $this->crud->set('show.setFromDb', false);
+    $this->crud->addColumns($this->getFieldsData(TRUE));
+}
+}
---
+++ fun/app/Http/Requests/TagRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+use Illuminate\Foundation\Http\FormRequest;
+
+class TagRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // only allow updates if the user is logged in
+        return backpack_auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:5|max:255',
+            'slug' => 'required|unique:tags'
+        ];
+    }
+
+    /**
+     * Get the validation attributes that apply to the request.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required' => 'Please provide valid name which is between 5 and 255 characters.',
+            'slug.required' => 'You should provide valid unique slug.',
+            'slug.unique' => 'The slug that you have provided has already been used by another tag.'
+        ];
+    }
+}
